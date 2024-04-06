89ae459635df41bafed6ab46b2972a3b734288c2
Surendra Vasamsetti <leelasurendravasamsetti@gmail.com>
2024-04-07 01:20:44 +0530

    modified uploads and others

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -26,17 +26,31 @@ const addFirm = async(req,res)=>{
       res.status(404).json({message: "vendor not found"})
     }
 
+    if(vendor.firm.length > 0){
+      return res.status(400).json({message: "vendor can have only one firm"});
+    }
+
     const firm = new Firm({
-      firmName, area, category, region, offer, image, vendor: vendor._id
+      firmName,
+    area,
+    category,
+    region,
+    offer,
+    image,
+     vendor: vendor._id
     })
 
     const savedFirm =await firm.save();
 
+    const firmId = savedFirm._id
+
     vendor.firm.push(savedFirm)
 
     await vendor.save();
+
+   
     
-    return res.status(200).json({message: "firm add successfully"})
+    return res.status(200).json({message: "firm add successfully", firmId})
 
   } catch (error) {
     console.error(error);
--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -43,10 +43,12 @@ const vendorLogin = async(req, res)=>{
       return res.status(401).json({error: "Invalid username or password"})
     }
 
-    const token = jwt.sign({vendorId : vendor._id}, secretKey, {expiresIn: "1h"})
+    const token = jwt.sign({vendorId : vendor._id}, secretKey, {expiresIn: "1h"});
 
-    res.status(200).json({success: "Login Successfull",token})
-    console.log(email);
+    const vendorId = vendor._id;
+
+    res.status(200).json({success: "Login Successfull",token, vendorId})
+    console.log(email, "this is token", token);
   } catch (error) {
     console.log(error);
     res.status(500).json({error: "Internal server error"});
@@ -71,7 +73,9 @@ const getVendorById = async(req,res) => {
     if(!vendor){
       return res.status(404).json({error: "vendor not found"})
     }
-    res.status(200).json({vendor})
+    const vendorFirmId = vendor.firm[0]._id;
+    res.status(200).json({vendorId, vendorFirmId, vendor})
+    console.log( vendorFirmId);
   }
   catch(error){
     console.log(error);
