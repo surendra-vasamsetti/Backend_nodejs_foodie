6ffe2410f45a1323e189638b5b49ac94aea96e98
Surendra Vasamsetti <leelasurendravasamsetti@gmail.com>
2024-04-14 18:55:24 +0530

    add_photos_uploads

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -75,3 +75,5 @@ const deleteFirmById = async(req, res)=>{
 }
 
 module.exports  = {addFirm: [upload.single('image'), addFirm], deleteFirmById}
+
+//surendra vasamsetti . iam from angara so, i would like to thank you first once because this is my first web application using react so i want to show my self for being this project so i try to learn first iam very loss confident . i try to learn something new so i decided to learn one of the new hype technology of web dev recently .then none other than react. so i want to try home i want to try it some some raect based components. it is a multi vendor project.multi vendor projof ect this multiple data so i am taking the data from the evening. just import the data from the begining like this you never be stop. i want to complete my project very soon asap.but i haven't ant interest in this . i donot know anything what i want to do. i want do the same thing as my own will in the future. 
